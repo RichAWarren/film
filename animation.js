c53729eea30fae1abf7ca55dcf945d3faf59d851
Richard Warren <rwarren92@hotmail.com>
2016-07-01 17:15:00 +0100

    cookie

--- animation.js
+++ animation.js
@@ -1,19 +1,10 @@
-window.addEventListener("load",function() {
-	// Set a timeout...
-	setTimeout(function(){
-		// Hide the address bar!
-		window.scrollTo(0, 1);
-	}, 0);
-});
-
-(function click() {
-    var loginClick = false;
+var loginClick = false;
 
 document.getElementById('login').addEventListener('click', function() {
     if (loginClick === false) {
-        document.getElementById('one').style.transform = "translate(-100px, 0px)";
-        document.getElementById('two').style.transform = "translate(100px, 0px)";
-        document.getElementById('three').style.transform = "translate(0px, 100px)";
+	    document.getElementById('one').style.transform = "translate(-100px, 0px)";
+	    document.getElementById('two').style.transform = "translate(100px, 0px)";
+	    document.getElementById('three').style.transform = "translate(0px, 100px)";
         loginClick = true;
     } else {
         document.getElementById('one').style.transform = "translate(0px, 0px)";
@@ -21,10 +12,12 @@ document.getElementById('login').addEventListener('click', function() {
         document.getElementById('three').style.transform = "translate(0px, 0px)";
         loginClick = false;
     }
-})
-
-})();
+});
 
 setTimeout(function(){
-    document.getElementById('pictureContainer').className += " animated swing";
+    document.getElementById('login').className += " animated swing";
 }, 1500)
+
+document.getElementById('one').addEventListener('click', function() {
+	document.cookie = "user=dan";
+});

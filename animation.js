540a4df430ba8e66af31f37d9a1f383077dd6d3c
Richard Warren <rwarren92@hotmail.com>
2016-07-01 15:10:04 +0100

    Login page

---
+++ animation.js
@@ -0,0 +1,22 @@
+(function () {
+    var loginClick = false;
+
+document.getElementById('login').addEventListener('click', function() {
+    if (loginClick === false) {
+        document.getElementById('one').style.transform = "translate(-100px, 0px)";
+        document.getElementById('two').style.transform = "translate(100px, 0px)";
+        document.getElementById('three').style.transform = "translate(0px, 100px)";
+        loginClick = true;
+    } else {
+        document.getElementById('one').style.transform = "translate(0px, 0px)";
+        document.getElementById('two').style.transform = "translate(0px, 0px)";
+        document.getElementById('three').style.transform = "translate(0px, 0px)";
+        loginClick = false;
+    }
+})
+
+})();
+
+setTimeout(function(){
+    document.getElementById('pictureContainer').className += " animated swing";
+}, 1500)
